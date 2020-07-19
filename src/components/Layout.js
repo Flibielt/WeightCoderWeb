094c100a7c8cd249cd001a3b36607012a2618c68
Csaba Hermann <hcsabi98@gmail.com>
2020-07-19 18:36:03 +0200

    Created Layout for default component

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from './header/Header'
+
+class Layout extends Component{
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Layout;
