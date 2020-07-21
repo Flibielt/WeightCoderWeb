d332ec1ede109c0c1149be48a23991f254c9d501
Csaba Hermann <hcsabi98@gmail.com>
2020-07-21 20:37:26 +0200

    Created the button design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,71 @@
 import React, {Component} from "react";
+import BootstrapSwitchButton from "bootstrap-switch-button-react/lib/bootstrap-switch-button-react";
 
 class Main extends Component {
     constructor(props, context) {
         super(props, context);
         this.state={
             decode: false,
+            mode: "Code",
             byteMode: 2,
             original: "",
             modified: ""
         }
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.original}
+                    onChange={(e)=>{
+                        this.setState({original: e.target.value});
+                    }}
+                />
+                <div className="container-fluid ml-0 mr-0">
+                    <div className="row">
+                        <div className="col-md-3 col-sm-6 pl-2 pr-2 text-center">
+                            <button type="button" className="btn btn-primary w-50">{this.state.mode}</button>
+                        </div>
+                        <div className="col-md-3 col-sm-6 pl-2 pr-2 text-center">
+                            <button type="button" className="btn btn-primary w-50">Clear</button>
+                        </div>
+                        <div className="col-md-3 col-sm-6 pl-2 pr-2 text-center">
+                            <BootstrapSwitchButton
+                                checked={this.state.decode}
+                                onstyle="primary w-50" offstyle="primary w-50" onlabel="Code" offlabel="Decode"
+                                onChange={(checked => {
+                                    this.setState({decode: checked});
+                                    if (checked) {
+                                        this.setState({mode: "Decode"})
+                                    } else {
+                                        this.setState({mode: "Code"})
+                                    }
+                                })}
+                            />
+                        </div>
+                        <div className="col-md-3 col-sm-6 pl-2 pr-2 text-center">
+                            <BootstrapSwitchButton
+                                checked={this.state.byteMode === 2}
+                                onstyle="primary w-50" offstyle="primary w-50" onlabel="2 byte" offlabel="1 byte"
+                                onChange={checked => {
+                                    if (checked) {
+                                        this.setState({byteMode: 2});
+                                    } else {
+                                        this.setState({byteMode: 1});
+                                    }
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    {this.state.modified}
+                </div>
+            </div>
+        )
     }
 }
 
