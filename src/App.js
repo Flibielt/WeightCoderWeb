4a5d7c8542fe81cf361ec182a4722c1f3420c42d
Csaba Hermann <hcsabi98@gmail.com>
2020-07-21 22:23:52 +0200

    Inserted the created components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import Layout from "./components/Layout";
 
-import './App.css';
-import "./App.scss"
+import "./App.scss";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <h1>Weight Coder</h1>
+        <Main/>
       </Layout>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
