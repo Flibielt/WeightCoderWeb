3bdab63a0b28d80e62ba6f787161c32ac9eac998
Csaba Hermann <hcsabi98@gmail.com>
2020-07-22 07:42:58 +0200

    Refactor

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
 import {Navbar} from "react-bootstrap";
 
 import logo from "../../img/illuminati.svg";
+import redLogo from "../../img/illuminati2.svg";
 
-const header = () => (
-    <Navbar className="navbar navbar-fixed-top bg-info" variant="dark">
-            <div className="col-1">
-                <img src={logo} alt="Logo" className="img-responsive w-75" />
-            </div>
-        <div className="col-11 text-left align-content-md-center text-xl-left">Weight coder</div>
-    </Navbar>
-)
+class Header extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state={
+            image: logo
+        }
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
 
-export default header;
+    handleMouseOver() {
+        this.setState({image: redLogo});
+    }
+
+    handleMouseOut() {
+        this.setState({image: logo});
+    }
+
+    render() {
+        return (
+            <Navbar className="navbar navbar-fixed-top bg-info" variant="dark">
+                <div className="col-1">
+                    <img src={this.state.image}
+                         alt="The truth is way heavier" title="The truth is way heavier"
+                         className="img-responsive w-75"
+                         onMouseOver={this.handleMouseOver}
+                         onMouseOut={this.handleMouseOut}
+                    />
+                </div>
+                <div className="col-11 text-left align-content-md-center text-xl-left">Weight coder</div>
+            </Navbar>
+        )
+    }
+}
+
+export default Header;
