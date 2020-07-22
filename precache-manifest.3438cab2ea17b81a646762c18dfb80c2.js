b906b1ee74e49b4f8bf3f2afe4a0e9fee02c0bc7
Csaba Hermann <hcsabi98@gmail.com>
2020-07-22 18:26:39 +0200

    Updates

---
+++ precache-manifest.3438cab2ea17b81a646762c18dfb80c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d85c4307308a7bbd4aeb6b2bd328de",
+    "url": "/WeightCoderWeb/index.html"
+  },
+  {
+    "revision": "70d915d9874e486d759b",
+    "url": "/WeightCoderWeb/static/css/main.5eaacea6.chunk.css"
+  },
+  {
+    "revision": "941dbcc9ea4821731e9e",
+    "url": "/WeightCoderWeb/static/js/2.3b51fda3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/WeightCoderWeb/static/js/2.3b51fda3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d915d9874e486d759b",
+    "url": "/WeightCoderWeb/static/js/main.4eb433c5.chunk.js"
+  },
+  {
+    "revision": "cd4597de60c939ba599c",
+    "url": "/WeightCoderWeb/static/js/runtime-main.10d6ff23.js"
+  },
+  {
+    "revision": "d367cf2923cf79094d57cefd7d3ad8e5",
+    "url": "/WeightCoderWeb/static/media/illuminati.d367cf29.svg"
+  },
+  {
+    "revision": "db330a08af4506d6ab68edfc5fb8223f",
+    "url": "/WeightCoderWeb/static/media/illuminati2.db330a08.svg"
+  }
+]);
